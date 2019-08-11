b260cbfac4041a5f702f1b53a9958c8d16e6b371
Mirko Cukich <mirkocukich@gmail.com>
2019-08-11 14:34:00 -0500

    Resources and Constraints placed on storyboards

--- Dark Theme FR/ViewController.swift
+++ Dark Theme FR/ViewController.swift
@@ -5,10 +5,14 @@
 //  Created by Mirko Cukich on 8/11/19.
 //  Copyright © 2019 Digital Mirko. All rights reserved.
 //
+// Swift Dark Theme First Responder Twitter Keyboard w/ Swipe Down Dismiss Demo - Demo 8 of 30
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Text View
+    @IBOutlet weak var textView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
